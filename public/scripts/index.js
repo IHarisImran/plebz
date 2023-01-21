c40bcad7b58c94353438dbdfdf7579bfc553b06f
Muhammad Haris <iharisimran@gmail.com>
2023-01-21 17:12:48 +0500

    contact,blog,sp page, left

--- public/scripts/index.js
+++
@@ -1,3 +0,0 @@
-// Fetching header & footer to all pages
-fetch('components/header.html').then(res => res.text()).then(data => { document.getElementById('header').innerHTML = data; });
-fetch('components/footer.html').then(res => res.text()).then(data => { document.getElementById('footer').innerHTML = data; });
